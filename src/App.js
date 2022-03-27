c4cc4dcaf24ce075e87cc22279ce4fa88c35fcb5
MGoldobin <misha2000goldobin@mail.ru>
2022-03-28 01:23:20 +0500

    add app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import './App.css';
+import React, { Component } from 'react'
+import List from './components/List'
+import Form from './components/Form'
+import styled from 'styled-components'
+import store from './store/index.js'
+import { observer } from 'mobx-react'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const Div = styled.div`
+  text-align: center;
+  margin: 30px auto;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
 
-export default App;
+const Buttons = styled.div`
+  display: flex;
+  width: 50%;
+  margin: 0 auto 30px;
+  justify-content: space-around;
+`
+const Content = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  gap: 20px;
+  width: 100%;
+  height: 80vh;
+`
+document.addEventListener('keydown', (e) => {
+  if(e.key==="Escape" || e.key==="Esc") store.zeroingWorker()
+})
+
+
+const App = observer(class extends Component {
+  render() {
+    return (
+      <Div className="app">
+        <Buttons>
+          <button>Добавить рабочего</button>
+          <button onClick={store.deleteWorker} disabled={store.worker.id ===null}>Удалить рабочего</button>
+        </Buttons>
+        <Content>
+          <List />
+          <Form />
+        </Content>
+      </Div>
+    )
+  }
+})
+
+export default App
