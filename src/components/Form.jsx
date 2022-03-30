0bd18966bb7269cfa8eda63990e7ce6fa971d445
MGoldobin <misha2000goldobin@mail.ru>
2022-03-30 16:17:29 +0500

    change keeping state logic

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { observer } from 'mobx-react'
 import styled from 'styled-components'
 import store from '../store'
+import { runInAction } from 'mobx'
 
 const StyledFrom = styled.form`
 	display: flex;
@@ -16,39 +17,34 @@ const StyledFrom = styled.form`
 const CenterInput = styled.div`
 	margin: 0 auto;
 `
-const ConditionalState = /*store.worker.id < store.max ? {
-	id: store.maxId,
-	name: '',
-	position: '',
-	birth: '',
-	gender: '',
-	isFired: false
-} : */{
-	id: store.worker.id,
-	name: store.worker.name,
-	position: store.worker.position,
-	birth: store.worker.birth,
-	gender: store.worker.gender,
-	isFired: store.worker.isFired
-} //big troubles*/
+
+const NonNullInput = styled.input`
+	border: 1px solid ${props => props.value.length ? 'black' : 'red'};
+`
+
+const NonNullSelect = styled.select`
+	border: 1px solid ${props => props.value.length ? 'black' : 'red'};
+	background-color: #fff;
+`
 
 const Form = observer(class extends Component {
 	constructor(props) {
     super(props)
-    this.state = ConditionalState
     this.handleChange = this.handleChange.bind(this)
   }
 
 	componentDidMount() {
 		document.addEventListener('keydown', (e) => {
-			if(e.key==="Escape" || e.key==="Esc") this.setState({
-				id: store.maxId,
-				name: '',
-				position: '',
-				birth: '',
-				gender: '',
-				isFired: false
-			})
+			if(e.key==="Escape" || e.key==="Esc") runInAction(() => 
+				store.changeWorker({
+					id: store.maxId,
+					name: '',
+					position: '',
+					birth: '',
+					gender: '',
+					isFired: false
+				})
+			)
 		})
 	}
 
@@ -57,33 +53,29 @@ const Form = observer(class extends Component {
 	}
 
 	handleChange(e) {
-		if(e.target.name === 'isFired') this.setState(prev => {return {isFired: !prev.isFired}})
-		else this.setState({[e.target.name]: e.target.value})
-		store.changeWorker({
-			id: store.maxId,
-			name: this.state.name,
-			position: this.state.position,
-			birth: this.state.birth,
-			gender: this.state.gender,
-			isFired: this.state.isFired
+		runInAction(() => {
+			if(e.target.name==='isFired') store.worker.isFired= !store.worker.isFired
+			else store.worker[e.target.name]= e.target.value
 		})
-		console.log(this.state, store.worker)
+		if(store.worker.id < store.maxId) store.changeWorker(store.worker)
 	}
 
 	render() {
 		return (
 			<StyledFrom>
-				<input 
+				<NonNullInput 
 					type="text" 
 					name="name"
 					placeholder='ФИО'
-					value={this.state.name} 
+					value={store.worker.name} 
 					onChange={e => this.handleChange(e)}
+					required
 				/>
-				<select
+				<NonNullSelect
 					name="position"
-          value={this.state.position}
+          value={store.worker.position}
 					onChange={e => this.handleChange(e)}
+					required
         >
           <option value="">Должность</option>
           <option value={'Junior Frontend'}>Junior Frontend</option>
@@ -92,27 +84,27 @@ const Form = observer(class extends Component {
 					<option value={'Junior Backend'}>Junior Backend</option>
 					<option value={'Middle Frontend'}>Middle Frontend</option>
 					<option value={'Senior Frontend'}>Senior Frontend</option>
-        </select>
+        </NonNullSelect>
 				<input 
 					name="birth"
         	label="Дата рождения"
         	type="date"
-        	value={this.state.birth}
+        	value={store.worker.birth}
 					onChange={e => this.handleChange(e)}
 					max="2004-01-01"></input>
 				<CenterInput>
 					<label htmlFor="gender">
 						М
-						<input type="radio" name="gender" value="male" onChange={e => this.handleChange(e)} checked={this.state.gender === "male"}/>	
+						<input type="radio" name="gender" value="male" onChange={e => this.handleChange(e)} checked={store.worker.gender === "male"}/>	
 					</label>
 					<label htmlFor="gender">
 						Ж
-						<input type="radio" name="gender" value="famale" onChange={e => this.handleChange(e)} checked={this.state.gender === "famale"}/>
+						<input type="radio" name="gender" value="famale" onChange={e => this.handleChange(e)} checked={store.worker.gender === "famale"}/>
 					</label>
 				</CenterInput>
 				<CenterInput>
 					Уволен
-					<input type="checkbox" name="isFired" value={this.state.isFired} checked={this.state.isFired} onChange={e => this.handleChange(e)}/>
+					<input type="checkbox" name="isFired" value={store.worker.isFired} checked={store.worker.isFired} onChange={e => this.handleChange(e)}/>
 				</CenterInput>
 				</StyledFrom>
 		)
